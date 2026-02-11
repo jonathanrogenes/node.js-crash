15046ae4afb98a772811f8072e2014cd9bde2d96
Jonathan Rogenes <jonathanrogenes@gmail.com>
2026-02-11 15:20:26 +0900

    default and normal export using es modules

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import getPosts from "./postController.js";
+import getPosts, { getPostsLength } from "./postController.js";
 console.log(getPosts());
-
+console.log(getPostsLength())
 
 // const { generateRandomNumber, celciusToF } = require('./utils');
 
