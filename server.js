7d0c8f82201c86bb62c5c10d0e162a4e20d5b23f
Jonathan Rogenes <jonathanrogenes@gmail.com>
2026-02-12 18:16:01 +0900

    req object

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT;
 const server = http.createServer((req, res) => {
   // res.setHeader('Content-Type', 'text/html');
   // res.statusCode = 404;
+  console.log(req.url)
+  console.log(req.method)
+
   res.writeHead(200, { 'Content-type': 'text/html'})
   res.end('<h1>Yo!</h1>')
 })
