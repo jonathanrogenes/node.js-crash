96028cb5cc00ca7615d59de9f2f8411faffb28a9
Jonathan Rogenes <jonathanrogenes@gmail.com>
2026-02-16 17:58:12 +0900

    get req body for POST. Used postman to confirm working

--- server2.js
+++ server2.js
@@ -46,6 +46,23 @@ const notFoundHandler = (req, res) => {
   res.end()
 }
 
+// Route handler for POST /api/users
+const createUserHandler = (req, res) => {
+  let body = ''
+  // Listen for data
+  req.on('data', (chunk) => {
+    body += chunk.toString()
+
+   })
+   req.on('end', () => {
+    const newUser = JSON.parse(body)
+    users.push(newUser)
+    res.statusCode = 201
+    res.write(JSON.stringify(newUser))
+    res.end()
+   })
+}
+
 const server = createServer((req, res) => {
   logger(req, res, () => {
     jsonMiddleware(req, res, () => {
@@ -53,6 +70,8 @@ const server = createServer((req, res) => {
         getUsersHandler(req, res)
       } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
         getUserByIdHandler(req, res)
+      } else if (req.url === '/api/users' && req.method === 'POST') {
+        createUserHandler(req,res)
       } else {
         notFoundHandler(req, res)
       }
