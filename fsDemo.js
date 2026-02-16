58e904225125c0eb4bae5ad713859e004972baeb
Jonathan Rogenes <jonathanrogenes@gmail.com>
2026-02-16 18:11:02 +0900

    File sysytem (fs) module

---
+++ fsDemo.js
@@ -0,0 +1,28 @@
+// import fs from 'fs'
+import fs from 'fs/promises'
+
+// readFile() - callback
+// fs.readFile('./test.txt', 'utf8', (err, data) => {
+//   if (err) throw err
+//   console.log(data)
+// })
+
+// readFileSync() - Synchronous version
+// const data = fs.readFileSync('./test.txt', 'utf8')
+// console.log(data)
+
+// readFile() - Promise version .then()
+// fs.readFile('./test.txt', 'utf8')
+// .then((data) => console.log(data))
+// .catch((err) => console.log(data))
+
+// readFile() - async/await
+const readFile = async () => {
+  try {
+    const data = await fs.readFile('./test.txt', 'utf8')
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+};
+readFile()
